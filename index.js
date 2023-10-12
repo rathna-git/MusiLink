923051fa31e667574dfc97d87af1eded85391c30
Rathna Chinthalapalli <hellorathnac@gmail.com>
2023-10-11 20:29:24 -0700

    express and local env setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const port = 8888;
+
+app.get('/', (req,res) => {
+    const data = {
+        name: 'Rathna',
+        isAwesome: true
+      };
+    
+      res.json(data);
+});
+
+app.get('/awesome-generator', (req, res) => {
+    const { name, isAwesome } = req.query;
+    res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
+  });
+
+app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`);
+});

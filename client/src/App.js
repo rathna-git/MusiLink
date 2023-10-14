b3e4ebf53ebc5e4105c089f96f167816dd0cc839
Rathna Chinthalapalli <hellorathnac@gmail.com>
2023-10-13 17:10:33 -0700

    refactor: move profile logic into seperate page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
 
-import { accessToken, getCurrentUserProfile, logout } from './Spotify';
+import { accessToken, logout } from './Spotify';
+import {Login, Profile} from './pages';
 import { GlobalStyle } from './styles';
 import styled from 'styled-components/macro';
-import { catchErrors } from './utils';
 
 
-const StyledLoginButton = styled.a`
-background-color: green;
-  color: white;
-  padding: 10px 20px;
-  margin: 20px auto;
-  border-radius: 30px;
-  display: inline-block;
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0,0,0,.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
 `;
 
 //Scroll to top of page when changing routes
@@ -31,28 +38,21 @@ function ScrollToTop(){
 function App() {
 
   const [token, setToken] = React.useState(null);
-  const [ profile, setProfile ] = React.useState(null);
 
   React.useEffect(() => {
    setToken(accessToken);
-
-   const fetchData = async() => {
-      const {data} = await getCurrentUserProfile();
-      setProfile(data);
-   };
-   catchErrors(fetchData());
   },[]);
 
 
   return (
-    <div className="App">
+    <div className="app">
       <GlobalStyle/>
-      <header className="App-header">
+ 
         {!token ? (
-          <StyledLoginButton href="http://localhost:8888/login" >
-            Log in to Spotify
-          </StyledLoginButton>
+         <Login />
         ): (
+          <>
+          <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
           <Router>
             <ScrollToTop />
              <Routes>
@@ -60,27 +60,12 @@ function App() {
                 <Route path='/top-tracks' element={<h1>Top Tracks</h1>}></Route>
                 <Route path='/playlists/:id' element={<h1>Playlist</h1>}></Route>
                 <Route path='/playlists' element= {<h1>Playlists</h1>}></Route>
-                <Route path='/' element= {
-                  <>
-                  <button onClick={logout}>Log Out</button>
-                  {profile && (
-                    <div>
-                        <h1>{profile.display_name}</h1>  
-                        <p>{profile.followers.total} Followers</p> 
-                        {profile.images.length && profile.images[0].url && (
-                          <img src={profile.images[0].url} alt="Avatar" />
-                        )}
-                    </div>
-                  )} 
-                  </>
-                }>
-                </Route>
+                <Route path='/' element= { <Profile />}></Route>
             </Routes>
           </Router>
-         
+         </>
         )}
 
-      </header>
     </div>
   );
 }

4b34c674d3537218c597513e3ba2c36fe25a09a1
Rathna Chinthalapalli <hellorathnac@gmail.com>
2023-10-17 15:06:23 -0700

    add toptracks page

---
+++ client/src/pages/TopTracks.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {getTopTracks} from '../Spotify';
+import { catchErrors } from "../utils";
+import { SectionWrapper, TrackList, TimeRangeButtons } from "../components";
+
+
+function TopTracks(){
+    const [topTracks,setTopTracks] = React.useState(null);
+    const [activeRange,setActiveRange] = React.useState('short');
+
+    React.useEffect(() => {
+        const fetchData = async() => {
+            const {data} = await getTopTracks(`${activeRange}_term`);
+            setTopTracks(data);
+        }
+
+        catchErrors(fetchData());
+    },[activeRange]);
+
+
+    return(
+        <main>
+            <SectionWrapper title="Top Tracks" breadcrumb={true}>
+                <TimeRangeButtons 
+                    activeRange={activeRange}
+                    setActiveRange={setActiveRange}
+                />
+                {topTracks && topTracks.items && (
+                    <TrackList tracks={topTracks.items} />
+                )}
+            </SectionWrapper>
+        </main>
+    );
+};
+
+export default TopTracks;

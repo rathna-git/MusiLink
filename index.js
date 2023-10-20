cf9139f217380e51757171d80e680280ea6a1b1a
Rathna Chinthalapalli <hellorathnac@gmail.com>
2023-10-20 11:44:15 -0700

    changed dependencies to fix axios 403 error - second try

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { error } = require('console');
 
 require('dotenv').config();
 
+
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;

dcf6d61cf985b0a6019c8111b8d3257789b40488
Rathna Chinthalapalli <hellorathnac@gmail.com>
2023-11-15 21:17:54 -0800

    add feature chart

---
+++ client/src/components/FeatureChart.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Chart from 'chart.js';
+
+import styled from 'styled-components';
+import {theme} from '../styles';
+const {fonts} = theme;
+
+const FeatureChart = props => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default FeatureChart;
--- client/src/pages/Track.js
+++ client/src/pages/Track.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types'
 import { getTrackInfo } from "../Spotify";
 import { formatDuration, getYear, parsePitchClass, catchErrors } from "../utils";
 
+import FeatureChart from "../components/FeatureChart";
+
 import styled from 'styled-components/macro';
+import {theme, mixins, media, Main} from '../styles';
+
+const {colors, fontSizes} = theme;
 
 const Track = () => {
  return(
